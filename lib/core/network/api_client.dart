e0de255f69fb11eaf5e63585c6ca562ab667b229
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2026-04-20 20:59:00 +0700

    second commit

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -26,8 +26,8 @@ class ApiClient {
 
     dio = Dio(BaseOptions(
       baseUrl: baseUrl,
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
